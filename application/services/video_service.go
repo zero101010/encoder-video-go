0e8063f725584679a378409c53c0ea4142f86c22
zero101010 <igotaraujo97@gmail.com>
2021-10-26 16:38:22 -0300

    finish encode method

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoder/application/repositories"
 	"encoder/domain"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -73,8 +72,6 @@ func (v *VideoService) Fragment() error {
 
 	cmd := exec.Command("mp4fragment", source, target)
 	output, err := cmd.CombinedOutput()
-	fmt.Println("hahahah------")
-	fmt.Println(output)
 	if err != nil {
 		return err
 	}
@@ -84,24 +81,48 @@ func (v *VideoService) Fragment() error {
 	return nil
 }
 
-// func (v *VideoService) Encode() error {
-// 	cmdArgs := []strings{}
-// 	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID+"frag")
-// 	cmdArgs = append(cmdArgs, "--use-segment-timeline")
-// 	cmdArgs = append(cmdArgs, "-o")
-// 	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID)
-// 	cmdArgs = append(cmdArgs, "-f")
-// 	cmdArgs = append(cmdArgs, "-exec-fir")
-// 	cmdArgs = append(cmdArgs, "/opt/bento4/bin")
-// 	cmd := exec.Command("mp4dash", cmdArgs...)
-// 	output, err := cmd.CombinedOutput()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	printOutput(output)
-// 	return nil
+// Pega o arquivo .frag e quebra em pedaços menores em mpegdash
+func (v *VideoService) Encode() error {
+	cmdArgs := []string{}
+	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID+".frag")
+	cmdArgs = append(cmdArgs, "--use-segment-timeline")
+	cmdArgs = append(cmdArgs, "-o")
+	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID)
+	cmdArgs = append(cmdArgs, "-f")
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/opt/bento4/bin/")
+	cmd := exec.Command("mp4dash", cmdArgs...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	printOutput(output)
+	return nil
 
-// }
+}
+
+// Remove arquivos temporários como .frag e .mp4
+func (v *VideoService) Finish() error {
+	err := os.Remove(os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		log.Println("Error removing mp4", v.Video.ID, ".mp4")
+		return err
+	}
+	err = os.Remove(os.Getenv("localStoragePath") + "/" + v.Video.ID + ".frag")
+	if err != nil {
+		log.Println("Error removing frag", v.Video.ID, ".frag")
+		return err
+	}
+	err = os.RemoveAll(os.Getenv("localStoragePath") + "/" + v.Video.ID)
+	if err != nil {
+		log.Println("Error removing dir", v.Video.ID)
+		return err
+	}
+	log.Println("Files have been removed: ", v.Video.ID)
+
+	return nil
+
+}
 
 // Verifica o output do comando mp4fragment
 func printOutput(out []byte) {
